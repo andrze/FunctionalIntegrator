ae3fa2788182f6f12d94aba72c5bfedc1131fa43
andrze <chlebickiandrzej@gmail.com>
2022-02-15 12:26:43 +0100

    Added synchronisation with 'mysz' server

--- sync.sh
+++ sync.sh
@@ -9,9 +9,29 @@
 
 # Sources
 # rsync -r -avz --prune-empty-dirs --exclude '*.html' --exclude 'obj/' --exclude '*.csv' --exclude '*.png' --exclude '.*' --exclude 'Release' --exclude 'Debug' . ac357729@kruk-host.fuw.edu.pl:~/Documents/FunctionalIntegrator
-make
-rsync -r -avz --prune-empty-dirs --exclude '*.nb' --exclude '*.html' --exclude 'obj' --exclude 'LINCENSE' --exclude 'makefile' --exclude '*.csv' --exclude '*.h' --exclude '*.cpp' --exclude '*.png' --exclude '.*' --exclude 'Release' --exclude 'Debug'  . ac357729@kruk-host.fuw.edu.pl:~/Documents/FunctionalIntegrator
+
+
+# Make the binary
+#make static
+
+options=(-avz --prune-empty-dirs )
+destination="ac357729@kruk-host.fuw.edu.pl:~/Documents/FunctionalIntegrator/"
+results="./results"
+if [ $# -gt 0 ] && [ "$1" == "mysz" ]
+then 
+    options+=('-e' 'ssh -A -J ac357729@kruk-host.fuw.edu.pl')
+    destination="achleb@10.42.11.24:~/FunctionalIntegrator/"
+    results="./results_mysz" 
+fi  
+
+echo "Synchronizing with ${destination}"
+
+rsync "${options[@]}" --exclude '*.nb' --exclude '*.html' --exclude 'obj' \
+    --exclude 'LINCENSE' --exclude '*.csv' --exclude 'FunctionalIntegrator' \
+    --exclude '*.png' --exclude '.*' --exclude 'Release' --exclude 'Debug'  \
+    . $destination
+#--exclude 'makefile' --exclude '*.h' --exclude '*.cpp' 
 
 # Results
-rsync -avz --include="*.csv" --exclude="*" ac357729@kruk-host.fuw.edu.pl:~/Documents/FunctionalIntegrator/ ./results
+rsync "${options[@]}" --include="*.csv" --exclude="*" $destination $results
 
