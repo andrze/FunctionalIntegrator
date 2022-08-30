6f66f02805224f6de71af36330c87dfff1781ba0
andrze <chlebickiandrzej@gmail.com>
2022-08-30 17:59:12 -0300

    Added the option for synchronization with cluster.uy

--- sync.sh
+++ sync.sh
@@ -12,7 +12,6 @@
 
 
 # Make the binary
-#make static
 
 options=(-avz --prune-empty-dirs )
 destination="ac357729@kruk-host.fuw.edu.pl:~/Documents/FunctionalIntegrator/"
@@ -22,12 +21,28 @@ then
     options+=('-e' 'ssh -A -J ac357729@kruk-host.fuw.edu.pl')
     destination="achleb@10.42.11.24:~/FunctionalIntegrator/"
     results="./results_mysz" 
+fi
+    
+if [ $# -gt 0 ] && [ "$1" == "cluster" ]
+then
+    destination="achlebicki@cluster.uy:/clusteruy/home/achlebicki/FunctionalIntegrator/"
+    results="./results_cluster" 
 fi  
+
+while ! [[ $confirm =~ [nNyY] ]]
+    do 
+	    read -p "Remake project? (y/n): " confirm
+    done
+    if [[ $confirm =~ [yY] ]]
+    then
+	    make clean
+	    make static
+    fi
 
 echo "Synchronizing with ${destination}"
 
 rsync "${options[@]}" --exclude '*.nb' --exclude '*.html' --exclude 'obj' \
-    --exclude 'LINCENSE' --exclude '*.csv' --exclude 'FunctionalIntegrator' \
+    --exclude 'LINCENSE' --exclude '*.csv' \
     --exclude '*.png' --exclude '.*' --exclude 'Release' --exclude 'Debug'  \
     . $destination
 #--exclude 'makefile' --exclude '*.h' --exclude '*.cpp' 
