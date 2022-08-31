e1e6b8bb5d880ce58d859deaf43b514321367c81
andrze <chlebickiandrzej@gmail.com>
2022-08-31 14:39:42 -0300

    Increased number of threads for small batches of jobs

--- makescripts_slurm.py
+++ makescripts_slurm.py
@@ -1,21 +1,6 @@
 from os import system
 from sys import argv
 
-# def options_dict(dim):
-#     at3D = 0.2
-#     at2D = 1.9
-#     if dim < 2:
-#         k = at2D
-#         r = 1.025
-#     elif dim <= 3:
-#         k = (3 - dim) * at2D + (dim - 2) * at3D
-#         r = (3 - dim) * 1.025 + (dim - 2) * 10
-#     else:
-#         k = at3D
-#         r = 10
-#
-#     return {'-dim': dim, '-out': 'flow_d_%1.2f.csv' % (dim), '-kappa': k, '-rhomax': r}
-
 test = False
 
 modes = ("critical", "single")
@@ -37,7 +22,7 @@ else:
 #            [{'-sigma_normalization': 'false'}]]
 #
 
-options = [[{'-kappa': .86 + i * 0.005} for i in range(0, 12)],
+options = [[{'-kappa': .88 + i * 0.0015} for i in range(1, 21)],
            [{'-u': 0.075}],
            [{'-dim': 2}],
            [{'-rhomax': 1.6}],
@@ -45,20 +30,9 @@ options = [[{'-kappa': .86 + i * 0.005} for i in range(0, 12)],
            [{'-precision': 1e-6}],
            [{'-norm_point': 0}],
            [{'-num_points': 160}],
-           [{'-a': 1.7 + i * .1}  for i in range(0, 5)],
+           [{'-a': 1.9}],
            [{'-N': 2}],
            [{'-sigma_normalization': 'false'}]]
-
-# options = [[{'-kappa': .50 + i * 0.02} for i in range(0, 4)],
-#            [{'-u': 1}],
-#            [{'-dim': 2}],
-#            [{'-rhomax': 1.2}],
-#            [{'-delta': 5e-6}],
-#            [{'-precision': 1e-6}],
-#            [{'-num_points': 120}],
-#            [{'-a': i * .1}  for i in range(10, 22)],
-#            [{'-N': 2}],
-#            [{'-sigma_normalization': 'false'}]]
 
 all_configurations = options[0]
 for single_option in options[1:]:
@@ -80,6 +54,8 @@ config_file = open('%s/scripts/run.config' % project_dir, 'w')
 MAX_RUNNING_JOBS = 30.
 MAX_CPUS = 80.
 MAX_TOTAL_JOBS = 50
+
+MAX_RUNNING_JOBS = min((len(all_configurations), MAX_RUNNING_JOBS))
 
 def submit_jobs():
     if len(all_configurations) > MAX_TOTAL_JOBS:
